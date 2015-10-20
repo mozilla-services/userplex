3e172cfc0481240656ba6fb8eda547e5356aef4e
Julien Vehent <julien@linuxwall.info>
2015-10-20 09:36:47 -0400

    Handle user deletion and key sorting in authorizedkeys module

--- modules/authorizedkeys/authorizedkeys.go
+++ modules/authorizedkeys/authorizedkeys.go
@@ -1,10 +1,12 @@
 package authorizedkeys
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/mozilla-services/userplex/modules"
@@ -25,10 +27,12 @@ func (m *module) NewRun(c modules.Configuration) modules.Runner {
 
 type run struct {
 	Conf modules.Configuration
+	p    parameters
 }
 
 type parameters struct {
-	Destination string
+	Destination    string
+	DeletionMarker string
 }
 
 func (r *run) Run() (err error) {
@@ -37,6 +41,7 @@ func (r *run) Run() (err error) {
 	if err != nil {
 		return err
 	}
+	r.p = params
 	users, err := r.Conf.LdapCli.GetUsersInGroups(r.Conf.LdapGroups)
 	if err != nil {
 		log.Fatal(err)
@@ -44,8 +49,16 @@ func (r *run) Run() (err error) {
 	// Gather all the uid and ssh pubkeys from ldap
 	userkeys := r.getUserKeys(users)
 
+	// If delete is set, flush existing files that match the deletionmarker
+	// (or all files that match if no marker is defined). The files will be
+	// recreate in the next step
+	err = r.deleteMarkedFiles()
+	if err != nil {
+		return
+	}
+
 	// Open one file descriptor per destination
-	destfiles := r.openDestFiles(params.Destination, userkeys)
+	destfiles := r.openDestFiles(userkeys)
 	defer func() {
 		for dest, _ := range destfiles {
 			destfiles[dest].Close()
@@ -53,7 +66,7 @@ func (r *run) Run() (err error) {
 	}()
 
 	// Write the pubkeys in the destfiles
-	r.writePubKeys(params.Destination, userkeys, destfiles)
+	r.writePubKeys(userkeys, destfiles)
 	return
 }
 
@@ -77,17 +90,51 @@ func (r *run) getUserKeys(users []string) (userkeys map[string][]string) {
 			log.Printf("[warning] authorizedkeys: %v", err)
 			continue
 		}
-		log.Printf("[info] found %d keys for user %s", len(userkeys[uid]), uid)
+		log.Printf("[info] found %d keys for user %q", len(userkeys[uid]), uid)
 
 	}
 	return
 }
 
-func (r *run) openDestFiles(d string, userkeys map[string][]string) (destfiles map[string]*os.File) {
+func (r *run) deleteMarkedFiles() (err error) {
+	if !r.Conf.Delete {
+		return
+	}
+	globber := strings.Replace(r.p.Destination, "{ldap:uid}", "*", -1)
+	files, err := filepath.Glob(globber)
+	if err != nil {
+		return
+	}
+	for _, file := range files {
+		fd, err := os.Open(file)
+		if err != nil {
+			log.Printf("[error] can't open %q for reading, skipping it", file)
+		}
+		defer fd.Close()
+		scanner := bufio.NewScanner(fd)
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), "deletionmarker="+r.p.DeletionMarker) {
+				if r.Conf.DryRun {
+					log.Printf("[dryrun] would have deleted %q", file)
+					continue
+				}
+				log.Printf("[info] flushing %q", file)
+				err = os.Remove(file)
+				if err != nil {
+					log.Printf("[error] failed to remove %q: %v", file, err)
+				}
+				continue
+			}
+		}
+	}
+	return
+}
+
+func (r *run) openDestFiles(userkeys map[string][]string) (destfiles map[string]*os.File) {
 	var err error
 	destfiles = make(map[string]*os.File)
 	for uid, _ := range userkeys {
-		dest := strings.Replace(d, "{ldap:uid}", uid, -1)
+		dest := strings.Replace(r.p.Destination, "{ldap:uid}", uid, -1)
 		if _, ok := destfiles[dest]; !ok {
 			if r.Conf.DryRun {
 				log.Println("[dryrun] would have created", dest)
@@ -96,28 +143,44 @@ func (r *run) openDestFiles(d string, userkeys map[string][]string) (destfiles m
 			os.MkdirAll(filepath.Dir(dest), 0750)
 			destfiles[dest], err = os.Create(dest)
 			if err != nil {
-				log.Printf("[warning] can't create %s: %v", dest, err)
+				log.Printf("[warning] can't create %q: %v", dest, err)
 			}
+			log.Printf("[info] creating %q", dest)
 		}
 	}
 	return
 }
 
-func (r *run) writePubKeys(d string, userkeys map[string][]string, destfiles map[string]*os.File) {
-	for uid, keys := range userkeys {
-		dest := strings.Replace(d, "{ldap:uid}", uid, -1)
-		fmt.Fprintf(destfiles[dest], "# %s LDAP pubkeys\n", uid)
-		if len(keys) == 0 {
+func (r *run) writePubKeys(userkeys map[string][]string, destfiles map[string]*os.File) {
+	var uids []string
+	for uid, _ := range userkeys {
+		uids = append(uids, uid)
+	}
+	sort.Strings(uids)
+	for _, uid := range uids {
+		dest := strings.Replace(r.p.Destination, "{ldap:uid}", uid, -1)
+		if r.Conf.DryRun {
+			log.Printf("[dryrun] would have written pubkey of %q into %q", uid, dest)
+			continue
+		}
+		marker := ""
+		if r.p.DeletionMarker != "" {
+			marker = fmt.Sprintf("; deletionmarker=%s", r.p.DeletionMarker)
+		}
+		fmt.Fprintf(destfiles[dest], "# %s LDAP pubkeys%s\n", uid, marker)
+		if len(userkeys[uid]) == 0 {
 			fmt.Fprintf(destfiles[dest], "# no key found in ldap, ask user to add one\n")
 			continue
 		}
+		var keys []string
+		for _, key := range userkeys[uid] {
+			keys = append(keys, key)
+		}
+		sort.Strings(keys)
 		for _, key := range keys {
-			if r.Conf.DryRun {
-				log.Printf("[dryrun] would have written pubkey of %s into %s", uid, dest)
-				continue
-			}
 			fmt.Fprintf(destfiles[dest], "%s\n", key)
 		}
+		log.Printf("[info] %d keys written into %q", len(userkeys[uid]), dest)
 	}
 	return
 }
